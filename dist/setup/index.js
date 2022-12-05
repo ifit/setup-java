98bafdce98cadcb710e74e53570ff2c2f157417d
Dmitry Shibanov <dmitry-shibanov@github.com>
2022-12-05 17:45:21 +0100

    Revert "Add support for Oracle JDK (#401)" (#421)
    This reverts commit e414b30daf813e3d843f6fa6325bd3a6db09075d.
    
    Co-authored-by: Brian Cristante <33549821+brcrista@users.noreply.github.com>

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -104157,7 +104157,6 @@ const installer_4 = __nccwpck_require__(8579);
 const installer_5 = __nccwpck_require__(883);
 const installer_6 = __nccwpck_require__(3613);
 const installer_7 = __nccwpck_require__(4750);
-const installer_8 = __nccwpck_require__(4298);
 var JavaDistribution;
 (function (JavaDistribution) {
     JavaDistribution["Adopt"] = "adopt";
@@ -104169,7 +104168,6 @@ var JavaDistribution;
     JavaDistribution["JdkFile"] = "jdkfile";
     JavaDistribution["Microsoft"] = "microsoft";
     JavaDistribution["Corretto"] = "corretto";
-    JavaDistribution["Oracle"] = "oracle";
 })(JavaDistribution || (JavaDistribution = {}));
 function getJavaDistribution(distributionName, installerOptions, jdkFile) {
     switch (distributionName) {
@@ -104190,8 +104188,6 @@ function getJavaDistribution(distributionName, installerOptions, jdkFile) {
             return new installer_6.MicrosoftDistributions(installerOptions);
         case JavaDistribution.Corretto:
             return new installer_7.CorrettoDistribution(installerOptions);
-        case JavaDistribution.Oracle:
-            return new installer_8.OracleDistribution(installerOptions);
         default:
             return null;
     }
@@ -104597,125 +104593,6 @@ class MicrosoftDistributions extends base_installer_1.JavaBase {
     }
 }
 exports.MicrosoftDistributions = MicrosoftDistributions;
-
-
-/***/ }),
-
-/***/ 4298:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.OracleDistribution = void 0;
-const core = __importStar(__nccwpck_require__(2186));
-const tc = __importStar(__nccwpck_require__(7784));
-const fs_1 = __importDefault(__nccwpck_require__(7147));
-const path_1 = __importDefault(__nccwpck_require__(1017));
-const base_installer_1 = __nccwpck_require__(9741);
-const util_1 = __nccwpck_require__(2629);
-const http_client_1 = __nccwpck_require__(9925);
-const ORACLE_DL_BASE = 'https://download.oracle.com/java';
-class OracleDistribution extends base_installer_1.JavaBase {
-    constructor(installerOptions) {
-        super('Oracle', installerOptions);
-    }
-    downloadTool(javaRelease) {
-        return __awaiter(this, void 0, void 0, function* () {
-            core.info(`Downloading Java ${javaRelease.version} (${this.distribution}) from ${javaRelease.url} ...`);
-            const javaArchivePath = yield tc.downloadTool(javaRelease.url);
-            core.info(`Extracting Java archive...`);
-            let extension = util_1.getDownloadArchiveExtension();
-            let extractedJavaPath = yield util_1.extractJdkFile(javaArchivePath, extension);
-            const archiveName = fs_1.default.readdirSync(extractedJavaPath)[0];
-            const archivePath = path_1.default.join(extractedJavaPath, archiveName);
-            const version = this.getToolcacheVersionName(javaRelease.version);
-            let javaPath = yield tc.cacheDir(archivePath, this.toolcacheFolderName, version, this.architecture);
-            return { version: javaRelease.version, path: javaPath };
-        });
-    }
-    findPackageForDownload(range) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const arch = this.distributionArchitecture();
-            if (arch !== 'x64' && arch !== 'aarch64') {
-                throw new Error(`Unsupported architecture: ${this.architecture}`);
-            }
-            if (!this.stable) {
-                throw new Error('Early access versions are not supported');
-            }
-            if (this.packageType !== 'jdk') {
-                throw new Error('Oracle JDK provides only the `jdk` package type');
-            }
-            const platform = this.getPlatform();
-            const extension = util_1.getDownloadArchiveExtension();
-            let major;
-            let fileUrl;
-            if (range.includes('.')) {
-                major = range.split('.')[0];
-                fileUrl = `${ORACLE_DL_BASE}/${major}/archive/jdk-${range}_${platform}-${arch}_bin.${extension}`;
-            }
-            else {
-                major = range;
-                fileUrl = `${ORACLE_DL_BASE}/${range}/latest/jdk-${range}_${platform}-${arch}_bin.${extension}`;
-            }
-            if (parseInt(major) < 17) {
-                throw new Error('Oracle JDK is only supported for JDK 17 and later');
-            }
-            const response = yield this.http.head(fileUrl);
-            if (response.message.statusCode === http_client_1.HttpCodes.NotFound) {
-                throw new Error(`Could not find Oracle JDK for SemVer ${range}`);
-            }
-            if (response.message.statusCode !== http_client_1.HttpCodes.OK) {
-                throw new Error(`Http request for Oracle JDK failed with status code: ${response.message.statusCode}`);
-            }
-            return { url: fileUrl, version: range };
-        });
-    }
-    getPlatform(platform = process.platform) {
-        switch (platform) {
-            case 'darwin':
-                return 'macos';
-            case 'win32':
-                return 'windows';
-            case 'linux':
-                return 'linux';
-            default:
-                throw new Error(`Platform '${platform}' is not supported. Supported platforms: 'linux', 'macos', 'windows'`);
-        }
-    }
-}
-exports.OracleDistribution = OracleDistribution;
 
 
 /***/ }),
